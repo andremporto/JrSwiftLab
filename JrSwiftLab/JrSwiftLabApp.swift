6d8e663f709fd27c37147cf045a49556863cc79b
André Porto <andreporto@me.com>
2023-11-14 15:13:33 -0300

    Initial Commit

---
+++ JrSwiftLab/JrSwiftLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  JrSwiftLabApp.swift
+//  JrSwiftLab
+//
+//  Created by André Porto on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct JrSwiftLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
