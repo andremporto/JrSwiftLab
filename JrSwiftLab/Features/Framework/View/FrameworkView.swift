23f70f500ebd63ba0c37fafe54c83bc708a5c726
Jonata Klabunde <jonata_k@yahoo.com.br>
2023-11-17 11:25:36 -0300

    structure changes

---
+++ JrSwiftLab/Features/Framework/View/FrameworkView.swift
@@ -0,0 +1,58 @@
+//
+//  FrameworkView.swift
+//  JrSwiftLab
+//
+//  Created by jonata klabunde on 17/11/23.
+//
+
+
+import SwiftUI
+
+struct FrameworkView: View {
+    
+    @ObservedObject var viewModel = FrameworkViewModel()
+    
+    var body: some View {
+        NavigationView {
+            List {
+                section("Roadmap", data: MockData.roadmap)
+                section("Base inicial", data: MockData.basic)
+                section("IDE", data: MockData.ide)
+                section("Versionamento de código", data: MockData.versioning)
+                section("Estudos", data: MockData.lessons)
+                section("Bibliotecas", data: MockData.frameworks)
+                section("Arquivos", data: MockData.files)
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    HStack {
+                        Image(systemName: "swift")
+                            .foregroundStyle(Gradient(colors: [.orange, .red]))
+                        Text("Jr. SwiftLab")
+                            .font(.title)
+                            .fontWeight(.bold)
+                    }
+                }
+            }
+        }
+        .accentColor(Color(.label))
+    }
+    
+    @ViewBuilder
+    func section(_ title: String, data: [Framework]) -> some View {
+        Section(title) {
+            ForEach(data) { framework in
+                NavigationLink(destination: FrameworkDetailView(framework: framework, isShowDetailView: $viewModel.isShowingDetailView)) {
+                    FrameworkTitleView(framework: framework)
+                }
+            }
+        }
+    }
+}
+
+
+#Preview {
+    FrameworkView()
+        .preferredColorScheme(.dark)
+}
